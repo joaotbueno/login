80504ab1cfb9d90be2f8931183476d085f453e69
João  Bueno <89426769+joaotbueno@users.noreply.github.com>
2022-06-08 20:27:38 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,5 @@
-// Criando um login com JavaScript HTML CSS  
+// Criando um evento para validação de usuário  
 
-/*     
-Fazer testes do tipo:
-
-- Selecionar e referenciar o e-mail e senha.
-- Criar funções utilizando a manipulação do DOM para entender melhor como que funciona as funções HTML.
-- Utilizar condições if else para criar ações na hora da validação do email e senha.
-- utilizar as funções do arquivo login_senhas.js.
-- executar funções que ajudem a dinamizar os inputs de email e senha.
-( validação, mudança de comportamento, adição de classe, mudança de texto do innerHTML, etc...).
-- Tentar conectar a pagina do arquivo menu.js em uma função que dirige o usuario a página
-caso o login esteja correto.
-- Integrar a página login.html com a alterar_senhas.html.
-
-*/
-
-// utilizando a função addEventListener, não precisa criar funções anteriores
-
-// NÃO EXCLUIR ESSE CÓDIGO POIS ESTÁ CORRETO !!!!
 
 
 let botao = document.querySelector('.button');
